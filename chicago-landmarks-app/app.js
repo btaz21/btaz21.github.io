dcda0b4131f2b4777058089321827d4409d11f63
Bobby Tazioli <bobby@Bobbys-MacBook-Pro.local>
2020-05-15 23:39:13 -0500

    updated css for the app with img and descriptive boxes

--- chicago-landmarks-app/app.js
+++ chicago-landmarks-app/app.js
@@ -5,31 +5,31 @@ $(() => {
 
 
     eraGenerator = (userInput) => {
-      const $h3 = $('<h3>')
-      $h3.text('Architectural styles of: ' + userInput)
+      const $h3 = $('<h3>').addClass('styles')
+      $h3.text('Year: ' + userInput)
       $('.periods').append($h3)
       if (userInput >= 1800 && userInput <= 1829) {
-        const architectureEra = $('<h3>')
+        const architectureEra = $('<h3>').addClass('styles')
         architectureEra.text('Queen Anne, Gothic Revival, Second Empire')
         $('.periods').append(architectureEra)
       }
       else if (userInput >= 1830 && userInput <= 1860) {
-        const architectureEra = $('<h3>')
+        const architectureEra = $('<h3>').addClass('styles')
         architectureEra.text('Greek Revival, Gothic Revival, Italianate, Exotic Revival, Egyptian Revival, Moorish Revival, Swiss Chalet, Octagon Mode')
         $('.periods').append(architectureEra)
       }
       else if (userInput >= 1861 && userInput <= 1890) {
-        const architectureEra = $('<h3>')
+        const architectureEra = $('<h3>').addClass('styles')
         architectureEra.text('International, Post-Modern Eclectic')
         $('.periods').append(architectureEra)
       }
       else if (userInput >= 1891 && userInput <= 1920) {
-        const architectureEra = $('<h3>')
+        const architectureEra = $('<h3>').addClass('styles')
         architectureEra.text('International, Post-Modern Eclectic')
         $('.periods').append(architectureEra)
       }
       else if (userInput >= 1891 && userInput <= 1920) {
-        const architectureEra = $('<h3>')
+        const architectureEra = $('<h3>').addClass('styles')
         architectureEra.text('International, Post-Modern Eclectic')
         $('.periods').append(architectureEra)
       }
@@ -38,8 +38,11 @@ $(() => {
 
 
     $('form').on('submit', (event) => {
-    event.preventDefault()
-    const userInput = $('input[type="text"]').val()
+      event.preventDefault()
+      $('.landmark-list').remove()
+      $('.styles').remove()
+      $('.info-items').remove()
+      const userInput = $('input[type="text"]').val()
 
     $.ajax(
       {
@@ -61,12 +64,17 @@ $(() => {
         $('.landmark-list').on('click', (event) => {
           const landmarkListItem = $(event.currentTarget).attr('id')
           console.log(landmarkListItem);
-          const $h3 = $('<h3>').text(data[landmarkListItem].landmark_name)
-          const $h4 = $('<h4>').text(data[landmarkListItem].address)
-          const $anotherh4 = $('<h4>').text('Architect: ' + data[landmarkListItem].architect)
+          const $h3 = $('<h3>').text(data[landmarkListItem].landmark_name).addClass('info-items')
+          const $h4 = $('<h4>').text(data[landmarkListItem].address).addClass('info-items')
+          const $anotherh4 = $('<h4>').text('Architect: ' + data[landmarkListItem].architect).addClass('info-items')
+          const $seeMoreLink = $('<h4>').text('See more').attr('id', 'seemore').addClass('info-items')
           $('.info').append($h3)
           $('.info').append($h4)
           $('.info').append($anotherh4)
+          $('.info').append($seeMoreLink)
+          $('#seemore').on('click', (event) => {
+            $('iframe').css('visibility', 'visible')
+          })
         })
       },
       (error) => {
@@ -88,3 +96,4 @@ $(() => {
 
 //References:
 //http://www.phmc.state.pa.us/portal/communities/architecture/styles/mid-19th-century.html
+//https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe
